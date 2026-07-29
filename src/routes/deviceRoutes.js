6f3a325822b71a3850f09fc5c7ffbce3244d0ef6
annumishra123 <anurag786332@gmail.com>
2026-07-29 09:58:01 +0530

    manage devies api

--- src/routes/deviceRoutes.js
+++ src/routes/deviceRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getDevices, removeDevice, logoutAllDevices } = require('../controllers/deviceController');
-const authMiddleware = require('../middleware/authMiddleware'); // tumhara existing JWT verify middleware
+const authMiddleware = require('../middleware/auth'); // tumhara existing JWT verify middleware
 
 router.get('/', authMiddleware, getDevices);
 router.delete('/:id', authMiddleware, removeDevice);
