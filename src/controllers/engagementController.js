36a5c5fd18594016a789fd8781f113b43388ae50
annumishra123 <anurag786332@gmail.com>
2026-07-30 15:20:29 +0530

    holidya  api

--- src/controllers/engagementController.js
+++ src/controllers/engagementController.js
@@ -50,7 +50,7 @@ exports.createTicket = asyncHandler(async (req, res) => {
 });
 
 exports.getMyTickets = asyncHandler(async (req, res) => {
-  const tickets = await Ticket.find({raisedBy: req._id}).sort('-createdAt');
+  const tickets = await Ticket.find({raisedBy: req.user._id}).sort('-createdAt');
   res.json({ success: true, data: tickets });
 });
 
