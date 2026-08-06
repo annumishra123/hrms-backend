ea0d0e9a46ec664e1ec9f6749c6b94583a46dac4
annumishra123 <anurag786332@gmail.com>
2026-08-06 17:20:39 +0530

    regularize api chnage some point

--- src/models/RegularizeRequest.js
+++ src/models/RegularizeRequest.js
@@ -4,7 +4,7 @@ const regularizeRequestSchema = new mongoose.Schema(
   {
     employee: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Employee',
+      ref: 'User',
       required: true,
     },
     date: {
